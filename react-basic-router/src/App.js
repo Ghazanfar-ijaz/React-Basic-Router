74ae69cc9ed3b9ae5044f7faa7be74b8cfe24fbf
Ghazanfar-ijaz <78793183+Ghazanfar-ijaz@users.noreply.github.com>
2021-05-18 00:05:55 +0200

    Update App.js

--- react-basic-router/src/App.js
+++ react-basic-router/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import { Switch, Route, Redirect, Link } from 'react-router-dom';
 import './App.css';
+
+import Home from './Pages/Home'
+import Project from './Pages/Project'
+import Contact from './Pages/Contact'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Link to='/'>Home</Link>
+      <Link to='/project'>Project</Link>
+      <Link to='/contact'>Contact</Link>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/project' component={Project} />
+        <Route exact path='/contact' component={Contact} />
+        <Redirect to='/' />
+      </Switch>
+    </Router>
   );
 }
 
-export default App;
+export default App
